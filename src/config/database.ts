2b903f9dfb6c6a8875b7d3b33b6e61a511e935be
Felipe Castillo <felipecastillo48@gmail.com>
2024-05-19 21:12:21 -0400

    chore:database configuration

---
+++ src/config/database.ts
@@ -0,0 +1,13 @@
+import pg from "pg";
+import { Kysely, PostgresDialect } from "kysely";
+import { Database } from "../types/schema";
+import { dbConnection } from "../common/constants.js";
+import "dotenv/config";
+
+const dialect = new PostgresDialect({
+  pool: new pg.Pool(dbConnection),
+});
+
+export const db = new Kysely<Database>({
+  dialect,
+});
