abea2d7da451d189cd70e511ef947dac73a77ed9
Felipe C <felipecastillo48@gmail.com>
2024-06-15 20:09:14 -0400

    chore: app bootstrap

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,16 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { ENVIRONMENT, PORT } from "./common/constants.js";
-
+import appRouter from "./routes/index.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-
+app.use("/api/v1", appRouter);
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to this world" });
+});
+app.get("*", (req, res) => {
+  res.status(404).json({message:"Not found"});
 });
 
 !ENVIRONMENT.TEST &&
