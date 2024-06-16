6ed72dbeb2965cd195c896759b67ccc3ac9eea90
Felipe Castillo <felipecastillo48@gmail.com>
2024-06-16 18:20:22 -0400

    chore(generic repository): generic repository
    This repository might be used as an abstract class or discarded in the future

--- src/repositories/numberPlateRepository.interface.ts
+++
@@ -1,9 +0,0 @@
-import {  NewNumberPlate, NumberPlateUpdate } from "../types/schema";
-
-export interface INumberPlateRepository<T> {
-    findById(plate:string):Promise<T>
-    findAll():Promise<T>
-    create(numberPlate: NewNumberPlate):Promise<T>
-    update(id:string, updateWith: NumberPlateUpdate):Promise<T>
-    delete(id:string):Promise<T>
-}
