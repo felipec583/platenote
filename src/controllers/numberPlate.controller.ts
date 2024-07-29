627c627ebbd2ada5545db9679c82b51db18f5bbc
Felipe Castillo <felipecastillo48@gmail.com>
2024-07-29 06:23:59 -0400

    chore: format  number plate related files

--- src/controllers/numberPlate.controller.ts
+++ src/controllers/numberPlate.controller.ts
@@ -1,30 +1,28 @@
 import { NumberPlateService } from "../services/numberPlate.service.js";
-import { Request, Response, } from "express";
 
+import { Request, Response } from "express";
 export class NumberPlateController {
-
   constructor(private readonly numberPlateService: NumberPlateService) {}
 
-  async getAllPlateNumbers(_req:Request, res:Response) {
+  async getAllPlateNumbers(_req: Request, res: Response) {
     const numberPlates = await this.numberPlateService.getAllNumberPlates();
     return res.status(200).json(numberPlates);
   }
 
-
-  
   async getPlateNumber(req: Request, res: Response) {
-    const {id} = req.params;
+    const { id } = req.params;
     console.log(id);
-    const foundNumberPlate = await this.numberPlateService.getNumberPlateByName(id);
+    const foundNumberPlate = await this.numberPlateService.getNumberPlateByName(
+      id
+    );
 
     return res.status(200).json(foundNumberPlate);
-
   }
 
-  async updateTenant(req:Request, res:Response) {
-    const {id}  = req.body;
-    const setIsTenant  = await this.numberPlateService.changeTenantStatus(id);
+  async updateTenant(req: Request, res: Response) {
+    const { id } = req.body;
+    const setIsTenant = await this.numberPlateService.changeTenantStatus(id);
     console.log(setIsTenant);
-    return res.status(200).send({"updatedTenant":"a"});
+    return res.status(200).send({ updatedTenant: "a" });
   }
 }
--- src/routes/numberPlate.route.ts
+++ src/routes/numberPlate.route.ts
@@ -1,10 +1,15 @@
 import Express from "express";
 import { numberPlateController } from "../controllers/dependencies.js";
 
-
 const router = Express.Router();
-router.get("/", numberPlateController.getAllPlateNumbers.bind(numberPlateController));
-router.get("/:id", numberPlateController.getPlateNumber.bind(numberPlateController));
+router.get(
+  "/",
+  numberPlateController.getAllPlateNumbers.bind(numberPlateController)
+);
+router.get(
+  "/:id",
+  numberPlateController.getPlateNumber.bind(numberPlateController)
+);
 router.put("/", numberPlateController.updateTenant.bind(numberPlateController));
 
 export default router;
