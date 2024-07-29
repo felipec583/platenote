8f0027a74396f1a233436a1abcb9900df8e0382e
Felipe Castillo <felipecastillo48@gmail.com>
2024-07-29 06:12:35 -0400

    chore: refine shift helper logic

--- src/common/helpers/getShift.ts
+++ src/common/helpers/getShift.ts
@@ -1,11 +1,12 @@
 import { TIME } from "../constants.js";
 
-const {START_EVENING, START_MORNING, END_EVENING, END_MORNING} = TIME;
+const { START_MORNING, END_MORNING} = TIME;
 
-export function getShift(): number | undefined  {
+export function getShift(): number  {
   const currentTime = new Date();
   const currentHour = +currentTime.getHours().toString();
   if (currentHour >= START_MORNING && currentHour <= END_MORNING) return 1;
-  if (currentHour >= START_EVENING && currentHour <= END_EVENING) return 2;
+  //if (currentHour >= START_EVENING && currentHour <= END_EVENING) 
+  return 2;
   
 }
