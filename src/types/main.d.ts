b92eea93904ded01e2f164ce435ca57f217bbd03
Felipe Castillo <felipecastillo48@gmail.com>
2024-09-05 08:09:53 -0400

    :wrench: chore: new type definitions
    request query, counter

--- src/types/main.d.ts
+++ src/types/main.d.ts
@@ -1,3 +1,47 @@
 import { Request, Response, NextFunction } from "express";
 
+import { BaseError } from "../common/helpers/error";
+
 type RequestHandler = (req: Request, res: Response, next: NextFunction) => void;
+
+type ErrorHandler = (
+  error: Error | BaseError,
+
+  req: Request,
+
+  res: Response,
+
+  next: NextFunction
+) => void;
+
+export interface RequestParams {}
+
+export interface ResponseBody {}
+
+export interface RequestBody {}
+
+type RequestQuery<T> = GenericQuery<T>;
+
+interface FindListsQuery {
+  shift: number | undefined;
+
+  start_date: string | undefined;
+
+  end_date: string | undefined;
+}
+
+export type FindListsRequestQuery = RequestQuery<FindListsQuery>;
+
+type GenericQuery<T> = {
+  [K in keyof T]: T[K];
+};
+
+interface Counter {
+  numberPlates: number;
+
+  hasLeft: number;
+
+  isRegistered: number;
+
+  isTenant: number;
+}
