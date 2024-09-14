c57546fc8d36df49420adb1f92f8a1519117d2a4
Felipe Castillo <felipecastillo48@gmail.com>
2024-09-14 07:20:22 -0300

    chore: :wrench: migration
    migration config and first migration to add index to number plate column

---
+++ src/migrations/2024-09-06_add_index_to_number_plate_table.ts
@@ -0,0 +1,14 @@
+import { Kysely } from "kysely";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export async function up(db: Kysely<any>) {
+  await db.schema
+    .createIndex("idx_number_plate")
+    .on("number_plate")
+    .column("number_plate")
+    .execute();
+}
+
+export async function down(db: Kysely<any>) {
+  await db.schema.dropIndex("idx_number_plate").on("number_plate").execute();
+}
