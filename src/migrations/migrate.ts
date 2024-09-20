ac10e256a8c65f8082e3f93a6536989b921f9ea7
Felipe Castillo <felipecastillo48@gmail.com>
2024-09-20 07:59:07 -0300

    style: :wrench: Whitespace deletion - migrate file

--- src/migrations/migrate.ts
+++ src/migrations/migrate.ts
@@ -2,8 +2,6 @@ import { Migrator, FileMigrationProvider } from "kysely";
 import { db } from "../config/database.js";
 import * as path from "path";
 import { promises as fs } from "fs";
-
-
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
