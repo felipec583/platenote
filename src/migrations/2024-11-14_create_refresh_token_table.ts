eb26a508383bac7dd149084653e948cce9cbaaea
Felipe Castillo <felipecastillo48@gmail.com>
2024-11-14 08:07:39 -0300

    chore: :card_file_box: migration: new refresh token table

---
+++ src/migrations/2024-11-14_create_refresh_token_table.ts
@@ -0,0 +1,18 @@
+import { Kysely } from "kysely";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable("refresh_token")
+    .addColumn("id", "uuid", (col) => col.primaryKey())
+    .addColumn("token", "varchar(255)", (col) => col.notNull().unique())
+    .addColumn("user_id", "uuid", (col) => col.references("user.id").notNull())
+    .addColumn("device_id", "uuid", (col) => col.notNull().unique())
+    .addColumn("issued_at", "timestamp", (col) => col.notNull())
+    .addColumn("expires_at", "timestamp", (col) => col.notNull())
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable("refresh_token").execute();
+}
