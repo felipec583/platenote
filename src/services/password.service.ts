5e15e1ce1410c1eca9357f0dd5c6e3ba2acacc59
Felipe Castillo <felipecastillo48@gmail.com>
2024-11-24 08:30:58 -0300

    feat: :zap: password service
    hashihg password, salting, and comparison

--- src/services/password.service.ts
+++ src/services/password.service.ts
@@ -1,9 +1,22 @@
-import { UserRepository } from "../repositories/user/user.repository";
-
+import { UserRepository } from "../repositories/";
+import bcrypt from "bcrypt";
 export class PasswordService {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async changePassword() {}
-  async validatePassword() {}
-  async hashPassword() {}
+  async validatePassword(password: string, hashedPassword: string) {
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+    return isMatch;
+  }
+
+  async hashPassword(password: string) {
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+  }
+
+  async updatePassword(userId: string, newPassword: string) {
+    const hashedPassword = await this.hashPassword(newPassword);
+    await this.userRepository.update(userId, { password: hashedPassword });
+  }
 }
