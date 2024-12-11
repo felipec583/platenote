44b7714018ac924a513df93d91629cf975e3e100
Felipe Castillo <felipecastillo48@gmail.com>
2024-12-11 07:53:52 -0300

    chore: :wrench: Middleware imports

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,16 @@ import cors from "cors";
 import "dotenv/config";
 import { ENVIRONMENT, PORT } from "./common/constants.js";
 import appRouter from "./routes/index.js";
-import { middleware } from "./middleware/index.js";
+import { errorMiddleware, loggerMiddleware } from "./middleware/index.js";
 export const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(middleware.loggerMiddleware);
+app.use(loggerMiddleware);
 app.use("/api/v1", appRouter);
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to this world" });
 });
-app.use(middleware.errorMiddleware);
+app.use(errorMiddleware);
 app.get("*", (req, res) => {
   res.status(404).json({ message: "Not found" });
 });
