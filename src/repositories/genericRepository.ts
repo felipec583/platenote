07d242d3f426b2b721ba4841ac4610c631bb4fe8
Felipe Castillo <felipecastillo48@gmail.com>
2024-12-30 20:15:23 -0300

    chore: generic repository: optional findAll

--- src/repositories/genericRepository.ts
+++ src/repositories/genericRepository.ts
@@ -7,7 +7,7 @@ U: UPDATE
 export interface Repository<R ,C, U> {
     create(entity:C):Promise<R>
     update(id:string , entity:U | string):Promise<R | object>
-    delete(id:string):Promise<R  |undefined>  
+    delete(id:string):Promise<R  |undefined>
     findById(id:string):Promise<object | undefined>
-    findAll():Promise<R | R[]>
+    findAll?():Promise<R | R[]>
 }
